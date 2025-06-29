1066b7034f793a4479b64e1f305f45280eebd53c
Vinicius A. <viniusername@gmail.com>
2025-06-29 19:19:03 -0300

    exercicio finalizado

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿const string FS = "tchau";
+
+Console.WriteLine("--- Groot Chatbot ---\n");
+
+string pergunta;
+do
+{
+    Console.Write("Pergunta: ");
+    pergunta = Console.ReadLine()!;
+
+    string resposta = ObtemRespostaGroot(pergunta);
+
+    ExibeResposta(ObtemRespostaGroot(pergunta));
+
+} while (pergunta.Trim().ToLower() != FS);
+
+
+void ExibeResposta(string resposta)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"                          Resposta: {resposta}");
+    Console.ResetColor();
+}
+
+string ObtemRespostaGroot(string pergunta)
+{
+    return pergunta.Trim().ToLower() == FS ?
+        "Eu sou Groot!" :
+        "Eu sou Groot.";
+}
